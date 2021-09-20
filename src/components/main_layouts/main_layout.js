647b79db20c16ab5db2020574d2dae28ccf38270
Astin <seokjun0915@icloud.com>
2021-09-21 08:46:55 +0900

    사이드 메뉴 추가

--- src/components/main_layouts/main_layout.js
+++ src/components/main_layouts/main_layout.js
@@ -24,10 +24,19 @@ const Layout = ({ pageTitle, children }) => {
                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
             </Helmet>
             <div className={styles.topMenu}>
-                <div className={styles.topLogo}><a href="#"><span className="material-icons" style={{fontSize:'30px'}}
-                onClick={event => {
-                    
-                }}>menu</span></a></div>
+                <a href="#">
+                    <span className="material-icons" style={{fontSize:'30px', margin: '22px 20px 20px 20px'}} onClick={event => {
+                        const sbar = document.getElementById('sidebar').style;
+                        const bg = document.getElementById('bg').style;
+                        bg.display = 'block';
+
+                        const innerWidth = window.innerWidth;
+                        sbar.width = innerWidth >= '768'? '375px' : '100%';
+                        sbar.display = 'block';
+                    }}>
+                        menu
+                    </span>
+                </a>
                 <nav style={{display: "flex", justifyContent: "center"}}>
                     <ul className={styles.navLinks}>
                         <li>
@@ -58,10 +67,56 @@ const Layout = ({ pageTitle, children }) => {
                     </ul>
                     <h2 className={styles.mobileLogo}>SJ_log</h2>
                 </nav>
-                <a href="#"><span className="material-icons" style={{fontSize: '30px', margin:'22px 20px 20px 20px'}}>search</span></a>
+                <a href="#">
+                    <span className="material-icons" style={{fontSize: '30px', margin:'22px 20px 20px 20px'}}>
+                        search
+                    </span>
+                </a>
             </div>
             <div className={styles.childrenContainer}>
                 {children}
+            </div>
+
+            <div className={styles.sideMenu} id='sidebar'>
+                <a href="#">
+                    <span className="material-icons" style={{fontSize: '30px', margin: '20px'}} onClick={event => {
+                        document.getElementById('sidebar').style.display = 'none';
+                        document.getElementById('bg').style.display = 'none';
+                    }}>
+                        close
+                    </span>
+                </a>
+                <p className={styles.sideLogo}>SJ_log</p>
+                <ul className={styles.sideLinks}>
+                    <li>
+                        <Link to="/" className={styles.navLinkText}>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className={styles.navLinkText}>
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/posts" className={styles.navLinkText}>
+                            Posts
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/photos" className={styles.navLinkText}>
+                            Photos
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/designs" className={styles.navLinkText}>
+                            Designs
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.background} id='bg'>
+
             </div>
         </div>
     )
