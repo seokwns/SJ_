b46f0bd73918a884f4ffb3dc97fd357ea8d077fc
Astin <seokjun0915@icloud.com>
2021-09-23 13:02:29 +0900

    아이콘 롤백
    : StaticImage 에서 placeholder 수정을 막아둬서 기존 방식으로 복귀

--- src/components/main_layouts/main_layout.js
+++ src/components/main_layouts/main_layout.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import * as styles from './main_layout.module.css'
 import { Helmet } from 'react-helmet'
-import { StaticImage } from 'gatsby-plugin-image';
+import { StaticImage } from 'gatsby-plugin-image'
 
 
 const links = [
@@ -45,28 +45,34 @@ const Layout = ({ pageTitle, children }) => {
             <Helmet>
                 <title>{pageTitle} | {data.site.siteMetadata.title}</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
                 <script>
                     document.body.style.minWidth = '340px';
                     document.body.style.overflowY = 'hidden';
                 </script>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
             </Helmet>
             <div className={styles.topMenu}>
-                <button>
-                    <StaticImage src='../../images/icons/outline_menu_black_36dp.png' alt='menu' onClick={() => {
-                        let sbar = document.getElementById('sidebar').style;
-                        let bg = document.getElementById('bg').style;
+                <button onClick={() => {
+                    let sbar = document.getElementById('sidebar').style;
+                    let bg = document.getElementById('bg').style;
+
+                    const innerWidth = window.innerWidth;
+                    if (innerWidth >= '768') {
+                        sbar.width = '375px';
                         bg.display = 'block';
+                    } else if (innerWidth < '768') {
+                        sbar.width = '100%';
+                        sbar.height = 'calc(100vh + 50px)';
+                    }
 
-                        const innerWidth = window.innerWidth;
-                        sbar.width = innerWidth >= '768'? '375px' : '100%';
-                        sbar.display = 'block';
+                    sbar.display = 'block';
 
-                        document.getElementById('bg').addEventListener('click', () => {
-                            document.getElementById('sidebar').style.display = 'none';
-                            document.getElementById('bg').style.display = 'none';
-                        });
-                    }} imgStyle={{width: '27px', height: '27px', margin: '20px'}} style={{width: '70px', height: '70px'}}/>
+                    document.getElementById('bg').addEventListener('click', () => {
+                        document.getElementById('sidebar').style.display = 'none';
+                        document.getElementById('bg').style.display = 'none';
+                    });
+                }}>
+                    <span className="material-icons" style={{fontSize:'30px', margin: '20px', color: 'black'}}>menu</span>
                 </button>
                 <nav style={{display: "flex", justifyContent: "center"}}>
                     <ul className={styles.navLinks}>
@@ -83,9 +89,7 @@ const Layout = ({ pageTitle, children }) => {
                     <h2 className={styles.mobileLogo}>SJ_log</h2>
                 </nav>
                 <button>
-                    <StaticImage src='../../images/icons/outline_search_black_36dp.png' alt='search' 
-                                 imgStyle={{width: '27px', height: '27px', margin: '22px 5px 18px 25px'}} 
-                                 style={{width: '70px', height: '70px'}}/>
+                <span className="material-icons" style={{fontSize:'30px', margin: '20px', color: 'black'}}>search</span>
                 </button>
             </div>
             <div className={styles.childrenContainer}>
@@ -93,14 +97,14 @@ const Layout = ({ pageTitle, children }) => {
             </div>
 
             <div className={styles.sideMenu} id='sidebar'>
-                <button>
-                    <StaticImage src='../../images/icons/outline_clear_black_36dp.png' alt='close' onClick={() => {
-                        document.getElementById('sidebar').style.display = 'none';
-                        document.getElementById('bg').style.display = 'none';
-                    }} imgStyle={{width: '27px', height: '27px', margin: '20px'}} style={{width: '70px', height: '70px'}}/>
+                <button onClick={() => {
+                    document.getElementById('sidebar').style.display = 'none';
+                    document.getElementById('bg').style.display = 'none';
+                }}>
+                    <span className="material-icons" style={{fontSize:'30px', margin: '20px', color: 'black'}}>clear</span>
                 </button>
                 <p className={styles.sideLogo}>SJ_log</p>
-                <nav style={{maxHeight: 'calc(100vh - 220px)', overflowY: 'auto'}}>
+                <nav style={{maxHeight: 'calc(100vh - 180px)', overflowY: 'auto'}}>
                     <ul className={styles.sideLinks}>
                     {
                         links.map(node => (
